57a0fa8e0f2180725223f35437c38fbb3823f0bb
Lalit Tyagi <lalittyagi1602@outlook.com>
2020-09-10 15:51:30 +0530

    Setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Main = () => {
+    return (
+        <h1>Clone of Slack</h1>
+    );
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
