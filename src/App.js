24a8642d1130764465d5246f12180864670a75bb
Lalit Tyagi <lalittyagi1602@outlook.com>
2020-09-10 18:50:50 +0530

    Done with header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css'
 
+import Header from './Header';
+
 function App() {
     return (
         <div className="App">
-            <h1>Clone of Slack</h1>
+            <Header/>
+            <div className="app_body">
+                {/* Sidebar */}
+                {/* React-Router -> Chat screen */}
+            </div>
         </div>
     );
 }
