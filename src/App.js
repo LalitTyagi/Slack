7804f0ad3bbc4c0c1dbef21e6bce8fd739246dbd
Lalit Tyagi <lalittyagi1602@outlook.com>
2020-09-11 16:58:09 +0530

    sidebar completed and db connection done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css'
 
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <div className="app_body">
-                {/* Sidebar */}
+                <Sidebar/>
                 {/* React-Router -> Chat screen */}
             </div>
         </div>
